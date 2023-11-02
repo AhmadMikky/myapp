69a128aef135914e349d244bc18a6330efbcf4ad
AhmadMikky <amm00081@mix.wvu.edu>
2023-11-02 19:58:00 -0400

    Initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+      <h1>CS230L</h1>   
+      <h2>Section-03</h2>  
+      <p>WVU ID 800378834</p>  
+      <p>Hi, my name is Ahmad Mikky</p>  
+    
+ 
+  </div>
   );
 }
 
